01c7e64e6a0d4a5dab6da57cf9a27fc02ef13e8a
Iriafen Ohiosikha <157080629+AIohio@users.noreply.github.com>
2025-04-26 18:19:47 -0400

    Update HoenScannerApplication.java

--- HoenScannerApplication.java
+++ HoenScannerApplication.java
@@ -1,8 +1,10 @@
 package com.skyscanner.hoenscanner;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.skyscanner.hoenscanner.model.SearchResult;
+
 import com.skyscanner.hoenscanner.resources.SearchResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
@@ -17,25 +19,29 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
     public static List<SearchResult> searchResults = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
+        
         new HoenScannerApplication().run(args);
     }
 
     @Override
     public void initialize(Bootstrap<HoenScannerConfiguration> bootstrap) {
-        // Nothing to do yet
+        // 
     }
 
     @Override
     public void run(HoenScannerConfiguration configuration, Environment environment) throws Exception {
+        
         ObjectMapper mapper = new ObjectMapper();
 
         InputStream rentalCarsStream = getClass().getClassLoader().getResourceAsStream("rental_cars.json");
         InputStream hotelsStream = getClass().getClassLoader().getResourceAsStream("hotels.json");
 
         if (rentalCarsStream != null) {
+            
             searchResults.addAll(mapper.readValue(rentalCarsStream, new TypeReference<List<SearchResult>>() {}));
         }
         if (hotelsStream != null) {
+            
             searchResults.addAll(mapper.readValue(hotelsStream, new TypeReference<List<SearchResult>>() {}));
         }
 
