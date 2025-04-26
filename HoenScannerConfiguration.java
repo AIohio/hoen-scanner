62534c2ac21ecdfd413136585f0e001fc0e1ae5e
Iriafen Ohiosikha <157080629+AIohio@users.noreply.github.com>
2025-04-26 18:20:10 -0400

    Update HoenScannerConfiguration.java

--- HoenScannerConfiguration.java
+++ HoenScannerConfiguration.java
@@ -3,5 +3,6 @@ package com.skyscanner.hoenscanner;
 import io.dropwizard.Configuration;
 
 public class HoenScannerConfiguration extends Configuration {
-    // No custom config needed
+    
+    
 }
